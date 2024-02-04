75296e8f1ec3e38d65a2c491192efae2c38a1621
le2Ks <quentin.neveu@hotmail.ca>
2024-02-03 22:36:46 -0500

    checkpoints

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -97,10 +97,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1707100888;
 
         // The best chain should have at least this much work.
-        //consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000006805c7318ce2736c0");
+        consensus.nMinimumChainWork = uint256S("0x000000000000000000000000000000000000000000000000000007197c6dc5ae");
 
         // By default assume that the signatures in ancestors of this block are valid.
-        //consensus.defaultAssumeValid = uint256S("0x1673fa904a93848eca83d5ca82c7af974511a7e640e22edc2976420744f2e56a"); //1155631
+        consensus.defaultAssumeValid = uint256S("0x3323f681923cc6156c051a420ccd09960b909d5684e7def12465d40f0c88995f");
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
@@ -144,29 +144,23 @@ public:
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
             (  0, uint256S("0x2a54867d32ac04d65197c00b8ce8b43e1c1e117fedd45609d90c76554f3ae3dc"))
-            //(  4032, uint256S("0x9ce90e427198fc0ef05e5905ce3503725b80e26afd35a987965fd7e3d9cf0846"))
-            //(  8064, uint256S("0xeb984353fc5190f210651f150c40b8a4bab9eeeff0b729fcb3987da694430d70"))
-            //( 16128, uint256S("0x602edf1859b7f9a6af809f1d9b0e6cb66fdc1d4d9dcd7a4bec03e12a1ccd153d"))
-            //( 23420, uint256S("0xd80fdf9ca81afd0bd2b2a90ac3a9fe547da58f2530ec874e978fce0b5101b507"))
-            //( 50000, uint256S("0x69dc37eb029b68f075a5012dcc0419c127672adb4f3a32882b2b3e71d07a20a6"))
-            //( 80000, uint256S("0x4fcb7c02f676a300503f49c764a89955a8f920b46a8cbecb4867182ecdb2e90a"))
-            //(120000, uint256S("0xbd9d26924f05f6daa7f0155f32828ec89e8e29cee9e7121b026a7a3552ac6131"))
-            //(161500, uint256S("0xdbe89880474f4bb4f75c227c77ba1cdc024991123b28b8418dbbf7798471ff43"))
-            //(179620, uint256S("0x2ad9c65c990ac00426d18e446e0fd7be2ffa69e9a7dcb28358a50b2b78b9f709"))
-            //(240000, uint256S("0x7140d1c4b4c2157ca217ee7636f24c9c73db39c4590c4e6eab2e3ea1555088aa"))
-            //(383640, uint256S("0x2b6809f094a9215bafc65eb3f110a35127a34be94b7d0590a096c3f126c6f364"))
-            //(409004, uint256S("0x487518d663d9f1fa08611d9395ad74d982b667fbdc0e77e9cf39b4f1355908a3"))
-            //(456000, uint256S("0xbf34f71cc6366cd487930d06be22f897e34ca6a40501ac7d401be32456372004"))
-            //(638902, uint256S("0x15238656e8ec63d28de29a8c75fcf3a5819afc953dcd9cc45cecc53baec74f38"))
-            //(721000, uint256S("0x198a7b4de1df9478e2463bd99d75b714eab235a2e63e741641dc8a759a9840e5"))
+            (  1, uint256S("0x174e5e88edc2dc5fe10c7b933919b11ede91208c8c4b02dcbd8bf9ea44d51ea8"))
+            (  16, uint256S("0xee384943280087aedf3e935c105eae4905f2b4fd6420356a3f102e1f4811f310"))
+            (  101, uint256S("0xd17c11f4a5423ada5a3eddad1830564f741dd8c971c03327b825e0a0e5674809"))
+            (  2156, uint256S("0xf3cc13e9d2f1bb7257d6a2f42a06dddde2931f0792cffceb94177d0a0f25e805"))
+            (  27599, uint256S("0x022f9c8dfc709ffd4138c63c5563ae56c572f37b588f48d5aee4c72aceac8b41"))
+            (  29280, uint256S("0xda9e6f8baad2bfbc94e2ff0a8e04deb6eb88cb9341ee7d9efaeee3e5e3381a30")) // BIPs34, 65, 65 | BIP9_CSV + Segwit activation process started
+            (  29520, uint256S("0x4e28e6922ccc1453420e8ddfee5c603fd4bf4a038336a9d1deea8ec02eda6231")) // BIPs34, 65, 65 | BIP9_CSV + Segwit activation process locked-in
+            (  29760, uint256S("0xfd7e0aa56ab9b4fac51668b74d866987b79f422ff93d72372a462a0989c51dc3")) // BIPs34, 65, 65 | BIP9_CSV + Segwit fully active
+            (  30000, uint256S("0x53279bc9aef5b117decce7b42d3d9b384f7cbfcbc8d0c74a3fcce42fe183e546")) // LWMA enabled
+            (  30038, uint256S("0x3323f681923cc6156c051a420ccd09960b909d5684e7def12465d40f0c88995f"))
         };
 
         chainTxData = ChainTxData{
-            // Data as of block b44bc5ae41d1be67227ba9ad875d7268aa86c965b1d64b47c35be6e8d5c352f4 (height 1155626).
-            1706913366, // * UNIX timestamp of last known number of transactions
-            0,  // * total number of transactions between genesis and that timestamp
+            1707015749, // * UNIX timestamp of last known number of transactions
+            30574,  // * total number of transactions between genesis and that timestamp
                     //   (the tx=... number in the SetBestChain debug.log lines)
-            0.06     // * estimated number of transactions per second after that timestamp
+            0.999869// * estimated number of transactions per second after that timestamp
         };
     }
 };
